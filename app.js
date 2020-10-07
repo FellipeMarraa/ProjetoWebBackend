e16896020262879a02a0f1674929c4f959d7bed5
FellipeMarrraa <fellipemarra@hotmail.com>
2020-10-06 22:23:48 -0300

    Implementando credencias

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const rotaTesteRouter = require('./routes/rotaTeste');
 
+
 var app = express();
 
 app.use(logger('dev'));
--- controllers/rotaTeste.js
+++ controllers/rotaTeste.js
@@ -1,6 +1,3 @@
-
-
-
 function resProcessaCalculo(re,res,next){
     console.log("Caiu no controller");
     res.status(200).send({"respostaCalculo":req.body.calculo})
